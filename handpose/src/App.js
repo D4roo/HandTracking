9451bb3b773001a949987e99f1a1358a389dcb72
Daroo <daramahmoudi@hotmail.com>
2021-07-29 13:27:02 -0700

    HandTrackingYay

--- handpose/src/App.js
+++ handpose/src/App.js
@@ -9,18 +9,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Webcam
+        ref={webcamRef}
+        style={{
+        position:"obsolute",
+        marginLeft:"auto",
+        marginRight:"auto",
+        left:0,
+        right:0,
+      textAlign:"center",
+        zIndex:9,
+        width:640,
+        height:480
+      }}
+      />
+      <canvas
+      ref={canvasRef}
+      style={{
+        position:"obsolute",
+        marginLeft:"auto",
+        marginRight:"auto",
+        left:0,
+        right:0,
+        textAlign:"center",
+        zIndex:9,
+        width:640,
+        height:480
+      }}
+      />
       </header>
     </div>
   );
