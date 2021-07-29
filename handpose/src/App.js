70c5ba7d7788513585c828894d5f6158d4a660ec
Daroo <daramahmoudi@hotmail.com>
2021-07-29 13:31:12 -0700

    asd

--- handpose/src/App.js
+++ handpose/src/App.js
@@ -1,7 +1,7 @@
 import React, {useRef} from 'react';
 //import logo from './logo.svg';
-import * as tf from "tensorflow/tfjs";
-import * as handpose from "tensorflow-models/handpose";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
 import Webcam from 'react-webcam';
 import './App.css';
 
